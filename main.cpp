f5d35c3c689a6e30de5d11974ebdbeac5574271b
Andres Rodriguez <andresssss10@gmail.com>
2018-04-12 17:57:47 -0500

    Vistas actualizadas

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-GLsizei winWidth=900, winHeight=900;
+GLsizei winWidth=1200, winHeight=900;
 //GLsizei winWidth=640, winHeight=480;            //set initial display window size
 GLfloat	rtri = 180.0f;				// Angle For The Triangle ( NEW )
 GLfloat	rquad = 45.0f;			// Angle For The Quad ( NEW )
@@ -21,11 +21,25 @@ void init(void) {
 
 void displayFcn(void){
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
+  //PARALELEPIPEDO FONDO
+  float l = 150.0f; // Longitud media de los lados del cuadrado
+  float d = 450.0f; //Logitud de la profundidad en z del paralelogramo
   glLoadIdentity();
-  glTranslatef(0.0f,0.0f,-700.0f);
+  //gluLookAt(-600.0, 0.0, -270.0f, 0.0f, 0.0, -270.0f, 0.0, 1.0f, 0.0); // Camara mirando del lado izquiedo
+  //gluLookAt(0.0, 0.0, 350.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
+  gluLookAt(0.0, 600.0, -270.0, 0.0, 0.0, -270.0, 0.0, 0.0,-1.0); // Camara mirando de arriba hacia abajo
+  glTranslatef(0.0f,0.0f,-500.0f);
   //glRotatef(rquad,0.0f,1.0f,0.0f);			// Rotate The Triangle On The Y axis ( NEW )
-  gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
-  float l = 110.0f; // Longitud media de los lados del cuadrado
+  //Rampa
+  glBegin(GL_QUADS);
+    glColor3f(0.789f, 0.0, 0.831f); //color violeta
+    glVertex3f(l/2, -l, 0.0f); //Vertice abajo derecha
+    glVertex3f(-l/2, -l, 0.0f); //Vertice abajo izquierda
+    glVertex3f(-l/2, -l, d); // Vertice abajo izquieda Punto
+    glVertex3f(l/2, -l, d); //Vertice abajo derecha punto
+  glEnd();
+
+
   glBegin(GL_QUADS);
     glColor3f(0.133f, 0.447f, 0.855f);  //Cambio de color azul
     glVertex3f(l, l, 0.0f);  //Vertice arriba derecha
@@ -33,18 +47,54 @@ void displayFcn(void){
     glVertex3f(-l, -l, 0.0f); //Vertice abajo izquierda
     glVertex3f(l, -l, 0.0f);  //Vertice abajo derecha
   glEnd();
-  glLoadIdentity();
-  glTranslatef(0.0f,0.0f,-700.0f);
+  //PARALELEPIPEDO LADO DERECHA
+  //glLoadIdentity();
+  //glTranslatef(0.0f,0.0f,-700.0f);
   //glRotatef(rquad,0.0f,1.0f,0.0f);			// Rotate The Triangle On The Y axis ( NEW )
-  gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
-  float d = 400.0f; //Logitud de la profundidad en z del paralelogramo
+  //gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
+
   glBegin(GL_QUADS);
-    glColor3f(0.216f, 0.467f, 0.482f);  //Cambio de color azul
+    glColor3f(0.216f, 0.467f, 0.482f);  //Cambio de color otro
     glVertex3f(l, l, 0.0f);  //Vertice arriba derecha
     glVertex3f(l, -l, 0.0f);  //Vertice abajo derecha
     glVertex3f(l, -l, d); //Vertice abajo derecha Punto
-    glVertex3f(l, l, d); //Vertice abajo izquierda
-
+    glVertex3f(l, l, d); //Vertice abajo izquierda punto
+  glEnd();
+  //PARALELEPIPEDO LADO ARRIBA SIN RELLENO
+  //glLoadIdentity();
+  //glTranslatef(0.0f,0.0f,-700.0f);
+  //glRotatef(rquad,0.0f,1.0f,0.0f);			// Rotate The Triangle On The Y axis ( NEW )
+  //gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
+  glBegin(GL_LINE_LOOP);
+    glColor3f(0.0f,1.0f,0.0f); //Cambio de color verde
+    glVertex3f(l, l, 0.0f);  //Vertice arriba derecha
+    glVertex3f(-l, l, 0.0f); //Vertice arriba izquieda
+    glVertex3f(-l, l, d);  //Vertice arriba izquierda punto
+    glVertex3f(l, l, d); //Vertice abajo derecha Punto
+  glEnd();
+  //PARALELEPIPEDO IZQUIEDA SIN RELLENO
+  //glLoadIdentity();
+  //glTranslatef(0.0f,0.0f,-700.0f);
+  //glRotatef(rquad,0.0f,1.0f,0.0f);			// Rotate The Triangle On The Y axis ( NEW )
+  //gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
+  glBegin(GL_LINE_LOOP);
+    glColor3f(0.0, 1.0f, 0.0f);  //Cambio de color verde
+    glVertex3f(-l, l, 0.0f);  //Vertice arriba izquieda
+    glVertex3f(-l, -l, 0.0f);  //Vertice abajo izquierda
+    glVertex3f(-l, -l, d); //Vertice abajo izquierda Punto
+    glVertex3f(-l, l, d); //Vertice arriba izquierda punto
+  glEnd();
+  //PARALELEPIPEDO ABAJO
+  //glLoadIdentity();
+  //glTranslatef(0.0f,0.0f,-700.0f);
+  //glRotatef(rquad,0.0f,1.0f,0.0f);			// Rotate The Triangle On The Y axis ( NEW )
+  //gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); // Camara mirando hacie eje -z
+  glBegin(GL_LINE_LOOP);
+    glColor3f(0.0, 1.0f, 0.0f);  //Cambio de color verde
+    glVertex3f(-l, -l, 0.0f);  //Vertice abajo izquierda
+    glVertex3f(l, -l, 0.0f);  //Vertice abajo derecha
+    glVertex3f(l, -l, d); //Vertice abajo derecha punto
+    glVertex3f(-l, -l, d); //Vertice abajo izquierda punto
   glEnd();
   /*glLoadIdentity();					// Reset The Current Modelview Matrix
   glTranslatef(-1.5f,0.0f,-10.0f);			// Move Left 1.5 Units And Into The Screen 6.0
